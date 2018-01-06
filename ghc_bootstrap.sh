e8c7bcc517ced85d4401269f67c323b1c9f16d1f
Peter Becich <peterbecich@gmail.com>
2018-01-05 21:11:28 -0800

    some prerequisites

--- ghc_bootstrap.sh
+++ ghc_bootstrap.sh
@@ -1,6 +1,13 @@
 
 # cues from https://github.com/fpco/stackage/blob/master/debian-bootstrap.sh
 
+apt-get update
+apt-get install -y \
+	build-essential \
+	cmake \
+	wget
+    
+
 wget https://downloads.haskell.org/~ghc/8.2.2/ghc-8.2.2-aarch64-deb8-linux.tar.xz
 
 tar xf ghc-8.2.2-aarch64-deb8-linux.tar.xz
